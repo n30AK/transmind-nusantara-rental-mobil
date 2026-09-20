fcd85519e775f9464ebb9d10bd8d89e42e21f9c0
n30AK <52341710+n30AK@users.noreply.github.com>
2026-09-20 08:14:15 +0700

    fix: clean Nexus SEO trust signals (#7)
    * fix: align SEO structured data with production contact and social identity
    
    * fix: align structured data contact and social identity

--- seo-schema.js
+++ seo-schema.js
@@ -6,7 +6,7 @@
     var data={
       '@context':'https://schema.org',
       '@graph':[
-        {'@type':'LocalBusiness','@id':'https://transmindnusantararentalmobil.co.id/#business','name':'Transmind Nusantara Rental Mobil','url':'https://transmindnusantararentalmobil.co.id/','telephone':'+628816654141','areaServed':['Bekasi','Jakarta','Bogor','Depok','Tangerang','Jabodetabek'],'priceRange':'$$','description':'Rental mobil Jabodetabek untuk lepas kunci, dengan driver, corporate, wedding dan pariwisata.'},
+        {'@type':'LocalBusiness','@id':'https://transmindnusantararentalmobil.co.id/#business','name':'Transmind Nusantara Rental Mobil','url':'https://transmindnusantararentalmobil.co.id/','telephone':'+6281292677888','areaServed':['Bekasi','Jakarta','Bogor','Depok','Tangerang','Jabodetabek'] ,'sameAs':['https://www.instagram.com/transmind.nusantara/','https://www.tiktok.com/@transmindrentalmobil','https://x.com/transmindnusant'],'priceRange':'$','description':'Rental mobil Jabodetabek untuk lepas kunci, dengan driver, corporate, wedding dan pariwisata.'},
         {'@type':'WebSite','@id':'https://transmindnusantararentalmobil.co.id/#website','url':'https://transmindnusantararentalmobil.co.id/','name':'Transmind Nusantara Rental Mobil','publisher':{'@id':'https://transmindnusantararentalmobil.co.id/#business'}},
         {'@type':'Service','name':'Rental Mobil Lepas Kunci','provider':{'@id':'https://transmindnusantararentalmobil.co.id/#business'},'areaServed':'Jabodetabek'},
         {'@type':'Service','name':'Rental Mobil Dengan Driver','provider':{'@id':'https://transmindnusantararentalmobil.co.id/#business'},'areaServed':'Jabodetabek'},
